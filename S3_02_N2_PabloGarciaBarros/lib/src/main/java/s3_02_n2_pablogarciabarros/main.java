47e84726e717b90a33e898bd9e16ba88e5679a1d
pgarcia86 <pgarciabarros86@gmail.com>
2023-02-16 15:31:27 +0100

    Primera subida

---
+++ S3_02_N2_PabloGarciaBarros/lib/src/main/java/s3_02_n2_pablogarciabarros/main.java
@@ -0,0 +1,10 @@
+package s3_02_n2_pablogarciabarros;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola");
+	}
+
+}
